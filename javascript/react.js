ecc64a31170f9dbc33be90a86128f38f4039eda1
GJoker <53223454+GJoker123@users.noreply.github.com>
2020-02-25 21:17:48 +0000

    Improved layout of website and added bootstrap

--- javascript/react.js
+++ javascript/react.js
@@ -3,47 +3,62 @@ function process() {
     var stringtext = document.getElementById("inputtext");
     var encrypttext = document.getElementById("ENC");
     var decrypttext = document.getElementById("DEC");
+
     stringtext = stringtext.value;
     var stringlength = stringtext.length;
     if (stringtext == "" || stringtext == " ") {
         alert("Please enter a sequence/letter/sentence into the bar.")
-    
-    }else {
-           if (encrypttext.checked) {
-        encrypt(stringlength, stringtext, n);
+
+    } else {
+        document.getElementById("spin").style.visibility = "visible";
+        if (encrypttext.checked) {
+            encrypt(stringlength, stringtext, n);
+        }
+        if (decrypttext.checked) {
+            decrypt(stringlength, stringtext, n);
+        }
     }
-    if (decrypttext.checked) {
-        decrypt(stringlength, stringtext, n);
-    } 
-        } 
-    
+
 }
 
-function Clear(){
+function Clear() {
     document.getElementById("inputtext").value = "";
+    document.getElementById("result").innerHTML = "";
+}
+
+function reset() {
+    document.getElementById("result").innerHTML = "";
 }
 
 function encrypt(stringlength, stringtext, n) {
-    var arr = Array.from(stringtext);
-    for (i = 0; i <= stringlength - 1; i++) {
-        arr[i] = arr[i].charCodeAt(0) + n;
-        arr[i] = String.fromCharCode(arr[i]); 
-    }
-    stringtext = arr.join("");
-    document.getElementById("inputtext").value = stringtext;
-    document.getElementById("result").innerHTML = stringtext;
+
+    setTimeout(function () {
+        document.getElementById("spin").style.visibility = "hidden";
+        var arr = Array.from(stringtext);
+        for (i = 0; i <= stringlength - 1; i++) {
+            arr[i] = arr[i].charCodeAt(0) + n;
+            arr[i] = String.fromCharCode(arr[i]);
+        }
+        stringtext = arr.join("");
+        document.getElementById("inputtext").value = stringtext;
+        document.getElementById("result").innerHTML = stringtext;
+    }, 1000)
+
     //alert("ENCTest completed");
 }
 //USE JSON
 function decrypt(stringlength, stringtext, n) {
-    var arr = Array.from(stringtext);
-    console.log(arr);
-    for (i = 0; i <= stringlength - 1; i++) {
-        arr[i] = arr[i].charCodeAt(0) - n;
-        arr[i] = String.fromCharCode(arr[i]);
-    }
-    stringtext = arr.join("");
-    document.getElementById("inputtext").value = stringtext;
-    document.getElementById("result").innerHTML = stringtext;
+    setTimeout(function () {
+        document.getElementById("spin").style.visibility = "hidden";
+        var arr = Array.from(stringtext);
+        for (i = 0; i <= stringlength - 1; i++) {
+            arr[i] = arr[i].charCodeAt(0) - n;
+            arr[i] = String.fromCharCode(arr[i]);
+        }
+        stringtext = arr.join("");
+        document.getElementById("inputtext").value = stringtext;
+        document.getElementById("result").innerHTML = stringtext;
+    },1000)
+
     //alert("DECTest completed");
 }
